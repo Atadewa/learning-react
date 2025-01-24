1d73fb4ff1557f52f086b6226f9c4cfb0d86895c
Atadewa <aditafli42@gmail.com>
2025-01-24 09:10:56 +0700

    fix: update feature stats

--- 4-shopping-notes/src/App.jsx
+++ 4-shopping-notes/src/App.jsx
@@ -149,6 +149,9 @@ function Item({ item, onDeleteItem, onToggleItem }){
 }
 
 function Footer({ items }){
+  if (items.length === 0) {
+    return <footer className="stats"> Your shopping list is empty. </footer>
+  }
   const totalItems = items.length;
   const checkedItems = items.filter((item) => item.checked === true).length;
   const percentage = Math.round(checkedItems / totalItems * 100);
